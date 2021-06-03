766a57ad84898845c497cb60ea23e5ae85fc642d
Eugene Markelov <noach4se@gmail.com>
2021-06-04 01:39:48 +0300

    lesson06 completed

--- index.js
+++ index.js
@@ -6,42 +6,36 @@ const isNumber = function (n) {
 
 const gameStart = function () {
   const number = Math.floor(Math.random() * 100)
-  let count = 3
-  let usernum = +prompt('Угадай число от 1 до 100'); // получим число введенное пользователем
+  let count = 10
+  let usernum = prompt('Угадай число от 1 до 100')
 
-  function play() {
-    console.log("🚀 ~answer", number)
+  const play = function () {
+    if (usernum === null) {
+      alert('Спасибо, что были с нами')
+      return
+    }
     if (!isNumber(+usernum) && +usernum !== null) {
-      console.log('is num', typeof usernum)
-      usernum = +prompt(`Введи число! ${count}`)
+      usernum = prompt(`Введи число! ${count}`)
       return play()
-    } else if (+usernum === null || typeof +usernum === 'string') {
-      alert('YOU ARE DONE HERE')
     }
     if (count > 0) {
       if (+usernum === number) {
         alert('Поздравляю, Вы угадали!!!');
       }
       else if (+usernum > number) {
-        count--;
         usernum = prompt(`Загаданное число меньше, осталось попыток ${count}`)
-        console.log('1', usernum, count)
-        if (usernum === null) {
-          alert('YOU ARE DONE HERE')
-        }
+        count--;
         return play()
       }
       else if (usernum < number) {
-        count--;
         usernum = prompt(`Загаданное число больше, осталось попыток ${count}`)
-        console.log('2', usernum, count)
+        count--;
         return play()
       } else {
         alert('Игра окончена!')
       }
     } else {
-      confirm('Попытки закончились, хотите сыграть еще?') ? gameStart() : alert('ЭТО КОНЕЦ')
-      console.log('3', count)
+      confirm('Попытки закончились, хотите сыграть еще?') ? gameStart() : alert('КОНЕЦ ИГРЫ')
     }
   }
   return play()
